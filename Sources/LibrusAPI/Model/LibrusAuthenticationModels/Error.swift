2f0c8f85182b68cf6140a86075416ce00f5d83d3
Oschly <iriusturar@icloud.com>
2020-05-02 09:13:41 +0200

    Add error struct
    Its presence is common when token is expired.

---
+++ Sources/LibrusAPI/Model/LibrusAuthenticationModels/Error.swift
@@ -0,0 +1,29 @@
+//
+//  Error.swift
+//  ddd
+//
+//  Created by Oskar on 02/05/2020.
+//  Copyright © 2020 Oschly. All rights reserved.
+//
+
+import Foundation
+
+struct ErrorJSON: Codable {
+  let status: JSONErrorStatus
+  let message: String
+  let code: ErrorCode
+  
+  enum CodingKeys: String, CodingKey {
+    case status = "Status"
+    case message = "Message"
+    case code = "Code"
+  }
+}
+
+enum JSONErrorStatus: String, Codable {
+  case error = "Error"
+}
+
+enum ErrorCode: String, Codable {
+  case tokenExpired = "TokenIsExpired"
+}
