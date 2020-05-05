bc3e789454d79bcd2eca660eef1dc489ff11d060
Oschly <iriusturar@icloud.com>
2020-05-05 09:34:34 +0200

    Remove commented code

--- Sources/LibrusAPI/Model/LibrusModels/BaseUser.swift
+++ Sources/LibrusAPI/Model/LibrusModels/BaseUser.swift
@@ -24,31 +24,3 @@ enum UserCodingKeys: String, CodingKey {
   case firstName = "FirstName"
   case lastName = "LastName"
 }
-
-//struct BaseUser: Codable {
-//  let id: Int
-//  
-//  let accountId: String
-//  
-//  let firstName: String
-//  
-//  let lastName: String
-//  
-//  init(from decoder: Decoder) {
-//    guard let container = try? decoder
-//      .container(keyedBy: UserCodingKeys.self)
-//      else { preconditionFailure() }
-//    
-//    self.accountId = try! container.decode(String.self, forKey: .accountId)
-//    self.firstName = try! container.decode(String.self, forKey: .firstName)
-//    self.lastName = try! container.decode(String.self, forKey: .lastName)
-//    self.id = try! container.decode(Int.self, forKey: .id)
-//  }
-//  
-//  init(id: Int, accountId: String, firstName: String, lastName: String) {
-//    self.id = id
-//    self.accountId = accountId
-//    self.firstName = firstName
-//    self.lastName = lastName
-//  }
-//}
