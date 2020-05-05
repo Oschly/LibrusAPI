8da9cd7ce1a3cf052525b980edd13c7c1500b9f3
Oschly <iriusturar@icloud.com>
2020-05-05 09:42:57 +0200

    Update Lesson struct for all required objects.

--- Sources/LibrusAPI/Model/LibrusModels/Lesson.swift
+++ Sources/LibrusAPI/Model/LibrusModels/Lesson.swift
@@ -7,22 +7,46 @@
 
 import Foundation
 
-struct Lesson: Codable, ShortFormed {
+struct Lesson: ShortFormed {
+  private enum CodingKeys: String, CodingKey {
+    case teacher = "Teacher"
+    case subject = "Subject"
+    case classRoom = "Class"
+  }
+  
   let id: Int?
   
   let url: URL?
   
+  let teacher: Teacher?
+  
+  let subject: Subject?
+  
+  let classRoom: Class?
+  
   init(from decoder: Decoder) {
-    guard let container = try? decoder
+    guard let shortFormedContainer = try? decoder
       .container(keyedBy: ShortFormedCodingKeys.self)
       else { preconditionFailure() }
     
-    self.id = try! container.decode(Int.self, forKey: .id)
-    self.url = URL(string: try! container.decode(String.self, forKey: .url))!
+    self.id = try! shortFormedContainer.decode(Int.self, forKey: .id)
+    
+    if let urlString = try? shortFormedContainer.decode(String.self, forKey: .url) {
+      self.url = URL(string: urlString)
+    } else {
+      self.url = nil
+    }
+    
+    guard let baseContainer = try? decoder
+      .container(keyedBy: CodingKeys.self)
+      else { preconditionFailure() }
+    
+    self.teacher = try? baseContainer.decode(Teacher.self, forKey: .teacher)
+    self.subject = try? baseContainer.decode(Subject.self, forKey: .subject)
+    self.classRoom = try? baseContainer.decode(Class.self, forKey: .classRoom)
   }
-  
-  func fetchedInfo(token: String) -> Lesson {
-    // TODO: Implement that
-    return self
-  }
+}
+
+extension Lesson: DecodableFromNestedJSON {
+  static var codingKey: ResponseKeys = .lesson
 }
