dadff8c503bf073040e5ab742138e17a4a50e377
Oschly <iriusturar@icloud.com>
2020-05-05 09:35:36 +0200

    Update CategoryColor for all required objects

--- Sources/LibrusAPI/Model/LibrusModels/CategoryColor.swift
+++ Sources/LibrusAPI/Model/LibrusModels/CategoryColor.swift
@@ -8,20 +8,36 @@
 import Foundation
 
 // TODO: - Implement ShortFormed
-struct CategoryColor: Codable {
+struct CategoryColor: DecodableFromNestedJSON {
+  private enum CodingKeys: String, CodingKey {
+    case rgb = "RGB"
+  }
+  
+  static var codingKey: ResponseKeys = .color
+  
   let id: Int?
   
   let url: URL?
   
+  let rgb: String?
+    
   init(from decoder: Decoder) {
-    guard let baseContainer = try? decoder.container(keyedBy: ShortFormedCodingKeys.self) else { preconditionFailure() }
+    guard let shortFormedContainer = try? decoder
+      .container(keyedBy: ShortFormedCodingKeys.self)
+      else { preconditionFailure() }
     
-    self.id = try? baseContainer.decode(Int.self, forKey: .id)
+    self.id = try? shortFormedContainer.decode(Int.self, forKey: .id)
     
-    if let stringUrl = try? baseContainer.decode(String.self, forKey: .url) {
+    if let stringUrl = try? shortFormedContainer.decode(String.self, forKey: .url) {
       self.url = URL(string: stringUrl)!
     } else {
       self.url = nil
     }
+    
+    guard let baseContainer = try? decoder
+      .container(keyedBy: CodingKeys.self)
+      else { preconditionFailure() }
+    
+    self.rgb = try? baseContainer.decode(String.self, forKey: .rgb)
   }
 }
