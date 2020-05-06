19b176c54bd67e713d0af75984e06a9406cbbe06
Oschly <iriusturar@icloud.com>
2020-05-06 12:34:58 +0200

    Add LessonRange struct
    Contains start time and end time of lesson. It's single object of array of all lessons' ranges for whole day.

---
+++ Sources/LibrusAPI/Extensions/Int+FormatToHour.swift
@@ -0,0 +1,17 @@
+//
+//  Int+FormatToHour.swift
+//  LibrusAPI
+//
+//  Created by Oskar on 06/05/2020.
+//
+
+import Foundation
+
+extension Int {
+  func toHourString() -> String {
+    let hour = self / 60
+    let minutes = self % 60
+    
+    return "\(hour):\(minutes)"
+  }
+}
---
+++ Sources/LibrusAPI/Model/LibrusModels/LessonRange.swift
@@ -0,0 +1,36 @@
+//
+//  LessonRange.swift
+//  LibrusAPI
+//
+//  Created by Oskar on 06/05/2020.
+//
+
+import Foundation
+
+struct LessonsRange: Codable {
+  private enum CodingKeys: String, CodingKey {
+    case rawStart = "RawFrom"
+    case rawEnd = "RawTo"
+  }
+  
+  /// Defined as minutes from midnight
+  let rawStart: Int
+  
+  /// Defined as minutes from midnight
+  let rawEnd: Int
+  
+  let start: String
+  let end: String
+  
+  init(from decoder: Decoder) {
+    guard let container = try? decoder
+      .container(keyedBy: CodingKeys.self)
+      else { preconditionFailure() }
+    
+    self.rawStart = try! container.decode(Int.self, forKey: .rawStart)
+    self.rawEnd = try! container.decode(Int.self, forKey: .rawEnd)
+    
+    self.start = rawStart.toHourString()
+    self.end = rawEnd.toHourString()
+  }
+}
